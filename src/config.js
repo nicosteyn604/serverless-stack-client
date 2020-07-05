cd94cfdcf3b809a18207cc9b5bfcae7614fcb76f
Nico Steyn <nico.steyn@solenta.com>
2020-07-05 19:55:38 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-central-1",
+        BUCKET: "com.solenta.aviation.notes-app-upload"
+    },
+    apiGateway: {
+        REGION: "eu-central-1",
+        URL: "https://p2seytuxbd.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-central-1",
+        USER_POOL_ID: "eu-central-1_ysmuL47oh",
+        APP_CLIENT_ID: "6e5j2kp51v3rm7o8nlol9isbna",
+        IDENTITY_POOL_ID: "u-central-1:de9e254e-1be1-4e1e-b5b5-6c02c8434c02"
+    }
+};
