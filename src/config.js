14c40e6de3a70faed391a2b23c293810144551c5
Nico Steyn <nico.steyn@solenta.com>
2020-07-06 10:32:17 +0200

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: "eu-central-1",
         BUCKET: "com.solenta.aviation.notes-app-upload"
@@ -11,6 +12,7 @@ export default {
         REGION: "eu-central-1",
         USER_POOL_ID: "eu-central-1_ysmuL47oh",
         APP_CLIENT_ID: "6e5j2kp51v3rm7o8nlol9isbna",
-        IDENTITY_POOL_ID: "u-central-1:de9e254e-1be1-4e1e-b5b5-6c02c8434c02"
-    }
+        IDENTITY_POOL_ID: "eu-central-1:de9e254e-1be1-4e1e-b5b5-6c02c8434c02"
+    },
+    STRIPE_KEY: "pk_test_51H1VGlI8ctnw0iEDfidr6XLLC7douqWflrycmXHySQzjuAL4DZ4jyHId2tA2PcBH4LCJpa055qOvvf4LaSmdayZS00MIv9Zu6e",
 };
