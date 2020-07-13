c2fce11fcf40baebb63a0463677d67ab1c267a78
Nico Steyn <nico.steyn@solenta.com>
2020-07-13 13:18:15 +0200

    Adding error reporting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { LinkContainer } from "react-router-bootstrap";
 import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
 import { onError } from "./libs/errorLib";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
     const [isAuthenticated, userHasAuthenticated] = useState(false);
@@ -71,11 +72,11 @@ function App() {
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-                <AppContext.Provider
-                    value={{ isAuthenticated, userHasAuthenticated }}
-                >
-                    <Routes />
-                </AppContext.Provider>
+                <ErrorBoundary>
+                    <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+                        <Routes />
+                    </AppContext.Provider>
+                </ErrorBoundary>
             </div>
         )
     );
--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,41 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.init({dsn: "https://0e8d7f8fa34f4a0294dff0814c7e1d79@o417001.ingest.sentry.io/5314771"});
+}
+
+export function logError(error, errorInfo = null) {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.withScope((scope) => {
+        errorInfo && scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    });
+}
+
 export function onError(error) {
+    let errorInfo = {};
     let message = error.toString();
 
     // Auth errors
     if (!(error instanceof Error) && error.message) {
+        errorInfo = error;
         message = error.message;
+        error = new Error(message);
+        // API errors
+    } else if (error.config && error.config.url) {
+        errorInfo.url = error.config.url;
     }
+
+    logError(error, errorInfo);
 
     alert(message);
 }
